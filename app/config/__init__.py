8f024bbc7e843936de9659e320de5611625be251
Bruno Cornejo <al_bcs96@hotmail.com>
2023-10-26 19:55:55 -0500

    Seañade configuraciones para el entorno de produccion

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -20,7 +20,13 @@ class DevelopmentConfig(BaseConfig):
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=1)
     MAIL_DEBUG = True
 
+class ProductionConfig(BaseConfig):
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=5)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=3)
+    MAIL_DEBUG = False
+
 
 environment = {
-    'development': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
 }
